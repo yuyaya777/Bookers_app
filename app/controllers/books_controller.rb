8b111ecc07570f3a30bd3b6ab78fe779d9724e57
yuyaya777 <yuyaya777@gmail.com>
2021-03-09 12:58:31 +0000

    Commit3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.permit(:title,:body)
   end
 end
