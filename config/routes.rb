91faea494b1649f7656cb2888db061f8d2f3cd5f
yuyaya777 <yuyaya777@gmail.com>
2021-03-10 07:08:19 +0000

    Commit6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  root :to => 'homes#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
