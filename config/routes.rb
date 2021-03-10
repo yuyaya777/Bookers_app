42df391ed7559dfb12d27e64bd28c129b2dd359e
yuyaya777 <yuyaya777@gmail.com>
2021-03-10 06:55:16 +0000

    Commit5

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
