664fbae9d9f198c4428dd07c9bab0443556ab98f
yuyaya777 <yuyaya777@gmail.com>
2021-03-11 11:22:16 +0000

    Commit12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,12 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
       flash[:alert] = "Book was successfully updated."
     else
-      redirect_to book_path(book.id)
+      render :edit
     end
   end
 
